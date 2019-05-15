a9d54eff3cbd40820e2f0f1b51e32cf9618914dc
deevkv <deev.kv@gmail.com>
2019-05-15 21:49:48 +0300

    Responsive pages mobile and tablet. Regulation menu and popup.

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -2,6 +2,7 @@
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var navOverlay = document.querySelector(".main-nav__overlay");
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -9,9 +10,11 @@ navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    navOverlay.classList.add('main-nav__show-overlay')
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    navOverlay.classList.remove("main-nav__show-overlay");
   }
 });
 
